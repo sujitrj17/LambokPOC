19099c03a6f70dc9ad61b3ff548e9bf909ec0771
sumasoft123 <sujit.jagdale@sumasoft.net>
2020-02-12 19:25:29 +0530

    First Commit

---
+++ src/main/java/com/LambokPOC/Lambok/LambokApplication.java
@@ -0,0 +1,13 @@
+package com.LambokPOC.Lambok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LambokPOC/Lambok/LambokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LambokPOC.Lambok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
